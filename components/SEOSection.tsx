18f5b81eac9a084f71b7aad7e9ed0dc34805bfaa
Rasmus Hag Løvstad <rasmus.hag.lovstad@gmail.com>
2024-04-04 10:17:04 +0200

    add social img to seo section

--- components/SEOSection.tsx
+++ components/SEOSection.tsx
@@ -1,10 +1,25 @@
 import Head from "next/head";
+import SocialIMG from "../public/social-img.png";
 
 export default function SEOSection() {
-  return <Head>
-    <meta property='og:title' content='Carbontracker: Measure carbon footprint of your ML models with ease.'/>
-    <meta property='og:description' content='Integrate Carbontracker into your codebase to predict and measure emissions during training.'/>
-    <meta property='og:url' content='//www.carbontracker.github.io/' />
-    {/* TODO: Insert plot as og:image */}
-  </Head>
+  return (
+    <Head>
+      <meta
+        property="og:title"
+        content="Measure carbon footprint of your ML models with ease."
+      />
+      <meta
+        property="og:title"
+        content="Carbontracker: Measure carbon footprint of your ML models with ease."
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="Integrate Carbontracker into your codebase to predict and measure emissions during training."
+      />
+      <meta property="og:url" content="//www.carbontracker.github.io/" />
+      <meta property="og:img" content={SocialIMG.src} />
+      {/* TODO: Insert plot as og:image */}
+    </Head>
+  );
 }
